7f9268265f27850bd633f4709b9a3eb10767675f
Muhammad-Sameer-Ansari <m.sameer.ansaree@gmail.com>
2021-10-09 17:10:04 +0500

    Task complete

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: (Scaffold(body: Text("Muhammad Sameer Ansari"))),
+    );
+  }
+}
+//https://github.com/Muhammad-Sameer-Ansari/Hellow-Flutter
